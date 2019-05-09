6372b633596bc3d61465a355a0504c25da2f961f
Tim Herzhauser <hellsing22@freenet.de>
2019-05-09 10:02:59 +0200

    Liste hinzugefügt

--- app/src/main/java/com/example/reiseplaner/MainActivity.java
+++ app/src/main/java/com/example/reiseplaner/MainActivity.java
@@ -2,6 +2,9 @@ package com.example.reiseplaner;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import java.util.ArrayList;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,4 +13,12 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+
+    public static final List<Reiseziel> REISEZIEL_LIST;
+
+    static {
+        REISEZIEL_LIST=new ArrayList<>();
+        REISEZIEL_LIST.add(new Reiseziel("Deutschland","Zweibrücken","HS-KL","09.05.2019","Hochschule"));
+        }
 }
