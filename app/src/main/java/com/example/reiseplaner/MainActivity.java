bff82d18a5a7ecd662d2cdd6be95e82e91c79c1e
Tim Herzhauser <hellsing22@freenet.de>
2019-05-09 10:42:20 +0200

    Test

--- app/src/main/java/com/example/reiseplaner/MainActivity.java
+++ app/src/main/java/com/example/reiseplaner/MainActivity.java
@@ -2,23 +2,32 @@ package com.example.reiseplaner;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.ArrayAdapter;
+
+
+
+
+
 import java.util.ArrayList;
 
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
+    ListView simpleList;
+    String countryList[] = {"India", "China", "australia", "Portugle", "America", "NewZealand"};
+
+    String Test[] = {"Deutschland","Frankreich","Österreich"};
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        simpleList = findViewById(R.id.geplanteReiseListe);
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<>(this, R.layout.activity_listview, R.id.textView, countryList);
+        simpleList.setAdapter(arrayAdapter);
     }
 
 
-    public static final List<Reiseziel> REISEZIEL_LIST;
-
-    static {
-        REISEZIEL_LIST=new ArrayList<>();
-        REISEZIEL_LIST.add(new Reiseziel("Deutschland","Zweibrücken","HS-KL","09.05.2019","Hochschule"));
-        }
 }
