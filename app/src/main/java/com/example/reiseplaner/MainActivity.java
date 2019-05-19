7ae11a4a5ae245978bb2d1aa36417dc6b3764bde
AZzalor <flippowei@googlemail.com>
2019-05-19 19:53:23 +0200

    liste

--- app/src/main/java/com/example/reiseplaner/MainActivity.java
+++ app/src/main/java/com/example/reiseplaner/MainActivity.java
@@ -2,42 +2,59 @@ package com.example.reiseplaner;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.BaseAdapter;
 import android.widget.ListView;
 import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
 import java.util.ArrayList;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
-    String Land[];
-    String Stadt[];
-
-    ListView simpleList;
-    String countryList[] = {"India", "China", "Australia", "Portugle", "America", "NewZealand"};
-    Reiseziel reiseziele[];
-
-
-
+    String[] land = {"Deutschland", "Frankreich"};
+    String[] stadt = {"Zweibrücken", "Paris"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        reiseziele[0] = new Reiseziel("Deutschland","Berlin","","","");
-        reiseziele[1] = new Reiseziel("Frankreich","Paris","","","");
 
+        ListView listView = (ListView) findViewById(R.id.geplanteReiseListe);
 
-        for (int i = 0; i < reiseziele.length; i++){
-            Land[i] = reiseziele[i].Land;
-            Stadt[i] = reiseziele[i].Stadt;
-        }
-
-        simpleList = findViewById(R.id.geplanteReiseListe);
-       CustomAdapter customAdapter = new CustomAdapter(getApplicationContext(), Land, Stadt);
-       simpleList.setAdapter(customAdapter);
-
-
+        CustomAdapter customAdapter = new CustomAdapter();
+        listView.setAdapter(customAdapter);
     }
 
+    class CustomAdapter extends BaseAdapter {
+        @Override
+        public int getCount() {
+            return land.length;
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return null;
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return 0;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            convertView = getLayoutInflater().inflate(R.layout.customlayout, null);
+            TextView textView_stadt = (TextView)convertView.findViewById(R.id.textView_stadt);
+            TextView textView_land = (TextView)convertView.findViewById(R.id.textView_land);
+
+            textView_land.setText(land[position]);
+            textView_stadt.setText(stadt[position]);
+
+            return convertView;
+        }
+    }
 }
