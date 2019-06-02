4d82b21b6bc8d0782627985fbf811befb0a52794
AZzalor <flippowei@googlemail.com>
2019-06-02 12:24:49 +0200

    erstellungsscreen

---
+++ app/src/main/java/com/example/reiseplaner/CreateScreen.java
@@ -0,0 +1,13 @@
+package com.example.reiseplaner;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CreateScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_screen);
+    }
+}
